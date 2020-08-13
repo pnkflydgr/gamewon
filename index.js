080cd1e77fbf7c3aa7a2312171d54a9c5512b5c5
pnkflydgr <tim.sonoftom@gmail.com>
2020-08-12 20:49:59 -0400

    move perrin to index

--- index.js
+++ index.js
@@ -1,79 +1,90 @@
-// FUNCTIONS
 const {
+  IsKeyDown,
+  KEY_DOWN,
+  KEY_LEFT,
+  KEY_RIGHT,
+  KEY_UP,
+  KEY_W,
+  KEY_A,
+  KEY_S,
+  KEY_D,
   InitWindow,
+  WHITE,
   SetTargetFPS,
   WindowShouldClose,
-  IsKeyDown,
   BeginDrawing,
   EndDrawing,
   CloseWindow,
+  DrawCircle,
+  BLACK,
   ClearBackground,
-  DrawCircle
-} = require("raylib")
+  RED,
+  BLUE,
+  GREEN,
+} = require("raylib");
+const frameRate = 60;
+const screenWidth = 500;
+const screenHeight = 500;
+const ballSize = 50;
+const ballSpeed = 10;
 
-// INPUT CONSTANTS
-const { KEY_DOWN, KEY_LEFT, KEY_UP, KEY_RIGHT } = require("raylib")
+InitWindow(screenWidth, screenHeight, "Super Cool Round Circle Game");
+let ballPositionX = screenWidth / 2;
+let ballPositionY = screenHeight / 2;
+SetTargetFPS(frameRate);
+const bgColor = WHITE;
+const colorArr = [BLUE, GREEN, RED, BLACK];
+let ballColor = colorArr[getRandomNum(0, 3)];
 
-// COLOR CONSTANTS
-const { RED, MAROON, BLACK, WHITE } = require("raylib")
+const randomizerRate = 5;
+let frameCounter = 0;
 
-// GAME LOCAL CONSTANTS
-// screen settings
-const screenWidth = 1200
-const screenHeight = 1000
-const frameRate = 60
-const bgColor = WHITE
-// ball settings
-let ballPositionX = screenWidth / 2
-let ballPositionY = screenHeight / 2
-const ballSize = 50
-const ballSpeed = 10
-let ballColorArray = [RED, BLACK, MAROON]
-let ballColor = ballColorArray[getRandomNum(0, 2)]
-// fun settings :)
-let frameCounter = 0
-const randomizerRate = 5 // this is seconds
-
-// INITIALIZATION OF GAME
-InitWindow(screenWidth, screenHeight, "Super Cool Round Circle Game")
-SetTargetFPS(60) // Set our game to run at 60 frames-per-second
-
-// MAIN GAME LOOP - THIS FIRES 60 TIMES PER SECOND
-// Detect window close button or ESC key
-while (!WindowShouldClose()) {
-  calculatePerimeter()
-  if (IsKeyDown(KEY_RIGHT) && ballPositionX < screenWidth - ballSize)
-    ballPositionX += ballSpeed
-  if (IsKeyDown(KEY_LEFT) && ballPositionX > ballSize)
-    ballPositionX -= ballSpeed
-  if (IsKeyDown(KEY_UP) && ballPositionY > ballSize) {
-    ballPositionY -= ballSpeed
-    console.log(ballPositionY)
+while (!WindowShouldClose())
+{
+  //myFunction(ballPositionX, ballPositionY)
+  if (
+    (IsKeyDown(KEY_RIGHT) || IsKeyDown(KEY_D)) &&
+    ballPositionX < screenWidth - ballSize
+  )
+    ballPositionX += ballSpeed;
+  if ((IsKeyDown(KEY_LEFT) || IsKeyDown(KEY_A)) && ballPositionX > ballSize)
+    ballPositionX -= ballSpeed;
+  if ((IsKeyDown(KEY_UP) || IsKeyDown(KEY_W)) && ballPositionY > ballSize)
+  {
+    ballPositionY -= ballSpeed;
   }
-  if (IsKeyDown(KEY_DOWN) && ballPositionY < screenHeight - ballSize)
-    ballPositionY += ballSpeed
+  if (
+    (IsKeyDown(KEY_DOWN) || IsKeyDown(KEY_S)) &&
+    ballPositionY < screenHeight - ballSize
+  )
+    ballPositionY += ballSpeed;
 
-  if (frameCounter++ % (frameRate * randomizerRate) == 0) {
-    ballPositionY = getRandomNum(ballSize, screenHeight - ballSize)
-    ballPositionX = getRandomNum(ballSize, screenWidth - ballSize)
-    ballColor = ballColorArray[getRandomNum(0, 2)]
+  if (frameCounter++ % (frameRate * randomizerRate) == 0)
+  {
+    ballColor = colorArr[getRandomNum(0, 3)];
+    ballPositionY = getRandomNum(ballSize, screenHeight - ballSize);
+    ballPositionX = getRandomNum(ballSize, screenWidth - ballSize);
+    frameCounter = 1;
   }
-  //---------------------------------------------------------------
-  // Draw
-  //---------------------------------------------------------------
-  BeginDrawing()
-  ClearBackground(bgColor)
-  DrawCircle(ballPositionX, ballPositionY, ballSize, ballColor)
-  EndDrawing()
-}
-CloseWindow() // Close window and OpenGL context
 
-function calculatePerimeter() {
-  //console.log(getRandomNum(-1, 3))
+  BeginDrawing();
+  ClearBackground(bgColor);
+  DrawCircle(ballPositionX, ballPositionY, ballSize, ballColor);
+  EndDrawing();
+}
+CloseWindow(); // Close window and OpenGL context
+
+function myFunction(ballPositionX, ballPositionY)
+{
+  //console.log("x: "+ ballPositionX)
+  //console.log("y: "+ ballPositionY)
+  let randomNum = getRandomNum(0, 10);
+  console.log(randomNum);
 }
 
-function getRandomNum(floor, ceil) {
-  const min = Math.ceil(ceil + 1)
-  const max = Math.floor(floor - 1)
-  return Math.floor(Math.random() * (max - min + 1)) + min
+function getRandomNum(floor, ceil)
+{
+  const max = Math.ceil(ceil + 1);
+  const min = Math.floor(floor - 1);
+  return Math.floor(Math.random() * (min - max + 1)) + max;
 }
